06e6e78adabdea795ef4e45e5094ff2a33da82fc
Mayna-B <me2be2014@gmail.com>
2023-09-24 11:50:14 -0400

    updated foster schema

--- models/foster.js
+++ models/foster.js
@@ -24,44 +24,7 @@ const fosterSchema = new mongoose.Schema({
     getsUpdates: {
         type: Boolean,
         default: false
-    },
-    password: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
-fosterSchema.pre("save", function (next) {
-    const user = this;
-    if (this.isModified("password") || this.isNew) {
-        bcrypt.genSalt(10, function (saltError, salt) {
-            if (saltError) {
-                return next(saltError);
-            }
-            else {
-                bcrypt.hash(user.password, salt, function (hashError, hash) {
-                    if (hashError) {
-                        return next(hashError);
-                    }
-                    user.password = hash;
-                    next();
-                });
-            }
-        });
-    }
-    else {
-        return next();
     }
 });
-fosterSchema.methods.comparePassword = function (password, callback) {
-    bcrypt.compare(password, this.password, function (error, isMatch) {
-        if (error) {
-            return callback(error);
-        }
-        else {
-            callback(null, isMatch);
-        }
-    });
-};
+
 module.exports = mongoose.model('foster', fosterSchema);
