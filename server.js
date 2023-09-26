8d61fbf918beef1b136658e66d865f293a4572b3
cpeppsi <caepps28@gmail.com>
2023-09-25 21:41:13 -0400

    Update seed data

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
-app.listen(PORT, console.log(`listining on port ${PORT}`))
+app.listen(PORT, console.log(`listening on port ${PORT}`))
 
 const petSeedData = [
     {
@@ -44,6 +44,30 @@ const petSeedData = [
         petType: "Dog",
         petAge: "3 Years",
         petBio: "Luna is a spirited, intelligent Border Collie mix with a heart full of love and a zest for life."
+    },
+    {
+        petName: "Midnight",
+        fosterName: "Ashton",
+        petGender: "Female",
+        petType: "Cat",
+        petAge: "9 Months",
+        petBio: "Midnight, a sleek and mysterious black cat, is a an enchanting companion who brings a touch of magic to every moment."
+    },
+    {
+        petName: "Max",
+        fosterName: "Jackson",
+        petGender: "Male",
+        petType: "Dog",
+        petAge: "5 Years",
+        petBio: "Max is a lovable Labrador Retriever, a wagging tail of happiness who's always up for an adventure and ready to be your faithful companion."
+    },
+    {
+        petName: "Mochi",
+        fosterName: "Allison",
+        petGender: "Male",
+        petType: "Cat",
+        petAge: "1 Year",
+        petBio: "Mochi, the beautiful calico cat with a patchwork of colors, is a charming and affectionate feline companion who will fill your life with love and colorful moments."
     }
 ];
 
